5331ea49df29f052a0da4778f1616bce28954523
luisflv <luisflv@gmail.com>
2014-12-13 11:46:50 -0200

    Adicionando Soluções - JQuery

---
+++ AplicacaoWeb/AplicacaoWebJQuery/js/aulaJQuery.js
@@ -0,0 +1,15 @@
+﻿$('#btnAlert').click(function () { alert('Boas férias!'); }); //EXIBE UM ALERT AO CLICAR
+
+//$('.tags').removeClass('tags'); //REMOVE O ESTILDO QUE FOI APLICADO A CADA ELEMENTO HTML
+
+$(document).ready(function () {
+   $('#btnOcultar').click(function () {
+      $('div').fadeOut(1000);
+   });
+
+   $('#btnExibir').click(function () {
+       $('div').fadeIn(1000);
+   });
+});
+
+
---
+++ AplicacaoWeb/Exercicio/js/exejquery.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('div').fadeOut(0.5);
+    $('#btnExibirFormulario').click(function () {
+        $('div').fadeIn(1000);
+    });
+}); //ESCONDE O FORMULÁRIO E EXIBE AO CLICAR NO BOTÃO EXIBIR FORMULÁRIO
+
